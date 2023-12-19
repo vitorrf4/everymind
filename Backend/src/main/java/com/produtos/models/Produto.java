23beba9b069d5c94b260bc99269888ddc160a7d6
vitorrf4 <vitorrf4@gmail.com>
2023-12-19 15:14:34 -0300

    feat: add produto class

---
+++ Backend/src/main/java/com/produtos/models/Produto.java
@@ -0,0 +1,56 @@
+package com.produtos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public Produto() { }
+
+    public Produto(String nome, String descricao, Double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
