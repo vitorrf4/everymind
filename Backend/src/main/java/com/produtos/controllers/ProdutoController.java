9ae10b07bdfd9d38512925ae6180a50882c0a20e
vitorrf4 <vitorrf4@gmail.com>
2023-12-19 16:46:26 -0300

    feat: add produto controller

---
+++ Backend/src/main/java/com/produtos/controllers/ProdutoController.java
@@ -0,0 +1,72 @@
+package com.produtos.controllers;
+
+import com.produtos.models.Produto;
+import com.produtos.models.ValidaProduto;
+import com.produtos.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/produtos")
+public class ProdutoController {
+    private final ProdutoRepository repository;
+
+    @Autowired
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getProdutos() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getProduto(@PathVariable Long id) {
+        var produto = repository.findById(id);
+
+        return produto.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> createProduto(@RequestBody Produto produto) {
+        if (ValidaProduto.camposSaoInvalidos(produto))
+            return ResponseEntity.badRequest().build();
+
+        Produto savedProduto = repository.save(produto);
+
+        URI produtoLocation = URI.create("produtos/" + savedProduto.getCodigo());
+
+        return ResponseEntity.created(produtoLocation).body(savedProduto);
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
+        if (produto.getCodigo() == null || ValidaProduto.camposSaoInvalidos(produto))
+            return ResponseEntity.badRequest().build();
+
+        if (!repository.existsById(produto.getCodigo()))
+            return ResponseEntity.notFound().build();
+
+        repository.save(produto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduto(@PathVariable Long id) {
+        var produtoDb = repository.findById(id);
+        if (produtoDb.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
