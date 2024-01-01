307a1dd9dfd1f4e89bf8c2474577580f919e7cba
vitorrf4 <vitorrf4@gmail.com>
2024-01-01 16:29:33 -0300

    refactor: update angular routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListaComponent} from "./components/lista/lista.component";
+import {CadastroComponent} from "./components/cadastro/cadastro.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "lista", component: ListaComponent },
+  { path: "cadastro", component: CadastroComponent },
+  { path: "**", redirectTo: "lista" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import { ListaComponent } from './components/lista/lista.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
