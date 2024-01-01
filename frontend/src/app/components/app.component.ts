b73d860f363ab4ceafbe7b6b5d8dc78cf469b69c
vitorrf4 <vitorrf4@gmail.com>
2024-01-01 16:56:01 -0300

    refactor: break down app code into other components

--- frontend/src/app/components/app.component.ts
+++ frontend/src/app/components/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import {ProdutosService} from "../services/produtos.service";
-import {Produto} from "../models/produto";
-import {Form, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,31 +6,5 @@ import {Form, FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  produtos : Produto[] = [];
-  formProduto! : FormGroup;
-
-  constructor(private produtosService: ProdutosService) {
-    this.formProduto = new FormGroup<any>({
-      nome: new FormControl(null),
-      descricao: new FormControl(null),
-      preco: new FormControl(null),
-    })
-
-    this.produtosService.listarTodos().subscribe(res => {
-      console.log(res);
-      this.produtos = res;
-    });
-  }
-
-  cadastrar() {
-    const produto = this.formProduto.value;
-
-    this.produtosService.cadastrar(produto).subscribe({
-      next: res => {
-        this.produtos.push(produto);
-        console.log("Produto cadastrado")
-      },
-      error: err => console.log(err)
-    });
-  }
+  constructor() { }
 }
