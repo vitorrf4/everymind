d8c1bdd8b6ee0766650d982be9310d6f6878a6f6
vitorrf4 <vitorrf4@gmail.com>
2024-01-01 16:21:19 -0300

    feat: add app header

--- frontend/src/app/components/app.component.ts
+++ frontend/src/app/components/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {ProdutosService} from "./services/produtos.service";
-import {Produto} from "./models/produto";
+import {ProdutosService} from "../services/produtos.service";
+import {Produto} from "../models/produto";
+import {Form, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,31 @@ import {Produto} from "./models/produto";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'produtos';
   produtos : Produto[] = [];
+  formProduto! : FormGroup;
 
   constructor(private produtosService: ProdutosService) {
-    this.listar();
-  }
+    this.formProduto = new FormGroup<any>({
+      nome: new FormControl(null),
+      descricao: new FormControl(null),
+      preco: new FormControl(null),
+    })
 
-  listar() {
     this.produtosService.listarTodos().subscribe(res => {
       console.log(res);
       this.produtos = res;
     });
-
   }
 
+  cadastrar() {
+    const produto = this.formProduto.value;
 
+    this.produtosService.cadastrar(produto).subscribe({
+      next: res => {
+        this.produtos.push(produto);
+        console.log("Produto cadastrado")
+      },
+      error: err => console.log(err)
+    });
+  }
 }
