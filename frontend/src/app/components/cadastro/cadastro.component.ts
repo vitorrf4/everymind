5227692d5e389877704a2f64c5e3a6a674fe12e7
vitorrf4 <vitorrf4@gmail.com>
2024-01-01 17:37:33 -0300

    refactor: update produto cadastro

--- frontend/src/app/components/cadastro/cadastro.component.ts
+++ frontend/src/app/components/cadastro/cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {Produto} from "../../models/produto";
 import {FormControl, FormGroup} from "@angular/forms";
 import {ProdutosService} from "../../services/produtos.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cadastro',
@@ -11,22 +11,43 @@ import {ProdutosService} from "../../services/produtos.service";
 export class CadastroComponent {
   formProduto! : FormGroup;
 
-  constructor(private produtosService: ProdutosService) {
+  constructor(private produtosService: ProdutosService,
+              private router: Router) {
     this.formProduto = new FormGroup<any>({
       nome: new FormControl(null),
       descricao: new FormControl(null),
       preco: new FormControl(null),
-    })
+    });
   }
 
-  cadastrar() {
+  cadastrarProduto() {
+    if (!this.formEstaValido()) return;
+
     const produto = this.formProduto.value;
 
     this.produtosService.cadastrar(produto).subscribe({
-      next: res => {
-        console.log("Produto cadastrado")
+      next: ()  => {
+        alert("Produto cadastrado com sucesso");
+        this.router.navigateByUrl("lista").then();
       },
       error: err => console.log(err)
     });
+  }
+
+  formEstaValido() : boolean {
+    if (!this.formProduto.get("nome")?.value ||
+      !this.formProduto.get("descricao")?.value ||
+      (!this.formProduto.get("preco")?.value && this.formProduto.get("preco")?.value != 0)
+    ) {
+      alert("Todos os campos devem ser preenchidos");
+      return false;
+    }
+
+    if (this.formProduto.get("preco")?.value < 0) {
+      alert("Preço inválido");
+      return false;
+    }
+
+    return true;
   }
 }
